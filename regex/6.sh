fcf37872498a5b992e65e41dde5e8cf6c741924b
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-11-06 12:01:32 -0300

    Correction

--- regex/6.sh
+++ regex/6.sh
@@ -2,16 +2,16 @@
 
 while read -r LINE
 do
-	if ! [ -z $(echo $LINE | grep -E '4[0-9]{3}-[0-9]{4}-[0-9]{4}-[0-9]{4}') ]
+	if ! [ -z $(echo $LINE | grep -E '^4[0-9]{3}-[0-9]{4}-[0-9]{4}-[0-9]{4}') ]
 	then
-		echo "Visa"
-	elif ! [ -z $(echo $LINE | grep -E '5[1-5][0-9]{2}-[0-9]{4}-[0-9]{4}-[0-9]{4}') ]
+		echo "$LINE:VISA"
+	elif ! [ -z $(echo $LINE | grep -E '^5[1-5][0-9]{2}-[0-9]{4}-[0-9]{4}-[0-9]{4}') ]
 	then
-		echo "Mastercard"
-	elif ! [ -z $(echo $LINE | grep -E '3[4-7][0-9]-[0-9]{4}-[0-9]{4}-[0-9]{4}') ]
+		echo "$LINE:MASTERCARD"
+	elif ! [ -z $(echo $LINE | grep -E '^3[4-7][0-9]-[0-9]{4}-[0-9]{4}-[0-9]{4}') ]
 	then
-		echo "American Express"
+		echo "$LINE:AMERICAN"
 	else
-		echo "Unknown"
+		echo "$LINE:UNKNOWN"
 	fi
 done < "debito-automatico.txt"
